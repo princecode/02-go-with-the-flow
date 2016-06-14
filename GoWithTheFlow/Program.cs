16a86e8fe1c66b9ef06ec9e9e3ff54fc9f6894c3
Prince <princemwilliams@gmail.com>
2016-06-14 16:16:12 -0400

    completed conditionals except issue with second null value. Added final catch all else statement.

--- GoWithTheFlow/Program.cs
+++ GoWithTheFlow/Program.cs
@@ -11,18 +11,8 @@ namespace GoWithTheFlow
         static void Main(string[] args)
         {
             Console.WriteLine("Lamp Usage Flow Chart");
-            //extra come back to this later
-
-            Console.WriteLine("Does your lamp work?");
-
-            string lampStat = Console.ReadLine();
-            if (lampStat == "yes")
-            {
-
-
-            }
-            //end of extra to be completed later
-            Console.WriteLine("Is the lamp plugged in? enter 'yes' or 'no'");
+           
+            Console.WriteLine("Is the lamp plugged in? Please enter 'yes' or 'no'");
 
             string plugged = Console.ReadLine();
             if (plugged.ToLower() == "yes");
@@ -32,46 +22,22 @@ namespace GoWithTheFlow
                 string burned = Console.ReadLine();
                 if (burned.ToLower() == "yes");
                 {
-                    Console.WriteLine("Replace bulb; and maybe try one of the new energy saving bulbs. They last for years");
+                    Console.WriteLine("Replace bulb; and maybe try one of the new energy saving bulbs. They last for years! :D");
 
-
-                }
-            }
+                             }
             else
             {
-                Console.WriteLine("Repair lamp... or even easier buy a new one! lol ;P");       
-            }
-
-            Console.WriteLine("What is height?");
-            double height = Convert.ToDouble(Console.ReadLine());
-
-            double pi = 3.14159;
-
-            if (radius > 0)
-            {
-                if (height > 0)
-                {
-                    // Volume: radius^2 * height* pi
-                    // we're doing radius * radius to mean radius^2
-                    double volume = (radius * radius) * height * pi;
-                    Console.WriteLine("The volume is " + volume);
-
-                    // Surface area: 2pi r * height + 2pi r^2
-                    double surfaceArea = (2 * pi * radius * height) + (2 * pi * radius * radius);
-                    Console.WriteLine("The surface area is " + surfaceArea);
-
+                Console.WriteLine("Repair lamp... or even easier buy a new one! lol ;P");
                 }
-                else
-                {
-                    Console.WriteLine("Height must not be negative!");
+                else 
+                { Console.WriteLine("You must enter 'yes' or 'no', please follow directions!");
                 }
-            }
-            else
-            {
-                Console.WriteLine("Radius cannot be negative!");
-            }
+                else 
+                { Console.WriteLine("You must enter 'yes' or 'no', please follow directions!");
+                        }
+                         
 
-
+     
             Console.ReadKey();
 
         }
